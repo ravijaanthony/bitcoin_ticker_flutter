a1ac47f5c32302c7b6c9bb875e98aa149c39f3d0
Anthony <ravija.20200624@iit.ac.lk>
2023-07-15 21:24:09 +0530

    Old code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,14 +6,12 @@ import 'package:flutter/material.dart';
 import 'coin_data.dart';
 
 // Future<Map<String, dynamic>> coinbaseValue = CoinbaseCurrencyValues().fetchBitcoinPrice();
-
 // CoinbaseCurrencyValues coinbaseCurrencyValues = CoinbaseCurrencyValues();
 ExchangeRates exchangeRates = ExchangeRates();
 // var assetIDQuote = exchangeRates.asset_id_quote;
 // var assetIDBase = exchangeRates.asset_id_base;
 
 String selectedCurrencyType = "USD";
-// var generate;
 //
 
 class PriceScreen extends StatefulWidget {
@@ -24,13 +22,11 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   DropdownButton androidDropdown() {
     List<DropdownMenuItem<String>> dropDownItems = [];
-    // List
     print(selectedCurrencyType);
 
     // fetchBitcoinPrice().then((data) {
     //   print(data['amount']);
     // });
-
     for (String value in currenciesList) {
       var newItem = DropdownMenuItem(
         child: Text(value),
@@ -38,17 +34,14 @@ class _PriceScreenState extends State<PriceScreen> {
       );
       dropDownItems.add(newItem);
     }
-
     return DropdownButton(
       value: selectedCurrencyType,
       items: dropDownItems,
       onChanged: (value) {
         asset_id_quote = value;
-
         setState(() {
           selectedCurrencyType = value!;
           exchangeRates.coinRates();
-          generatingWidget();
           // fetchBitcoinPrice();
           // coinbaseCurrencyValues;
         });
@@ -58,11 +51,9 @@ class _PriceScreenState extends State<PriceScreen> {
 
   CupertinoPicker iOSPicker() {
     List<Text> pickerItemList = [];
-
     for (String currency in currenciesList) {
       pickerItemList.add(Text(currency));
     }
-
     return CupertinoPicker(
         backgroundColor: Colors.lightBlue,
         itemExtent: 32,
@@ -82,74 +73,107 @@ class _PriceScreenState extends State<PriceScreen> {
     return iOSPicker();
   }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   generate = generatingWidget();
+  // String textValue(){}
+  // threeWidgets(String textValue) {
+  //   var value;
+  //   // loop 3 times
+  //   String finalCurrencyValue = exchangeRates.formattedNumberCurrency;
+  //
+  //   for (String currency in cryptoList) {
+  //     return value = currency;
+  //   }
+  //   Container(
+  //     color: Colors.lightBlueAccent,
+  //     child: Padding(
+  //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+  //       child: Text(
+  //         '1 $value = $finalCurrencyValue $asset_id_quote',
+  //         textAlign: TextAlign.center,
+  //         style: TextStyle(
+  //           fontSize: 20.0,
+  //           color: Colors.white,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  //   Card(
+  //     color: Colors.lightBlueAccent,
+  //     elevation: 5.0,
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //     ),
+  //     child: Padding(
+  //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+  //       child: Text(
+  //         '1 $value = $finalCurrencyValue $asset_id_quote',
+  //         textAlign: TextAlign.center,
+  //         style: TextStyle(
+  //           fontSize: 20.0,
+  //           color: Colors.white,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  //   Card(
+  //     color: Colors.lightBlueAccent,
+  //     elevation: 5.0,
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //     ),
+  //     child: Padding(
+  //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+  //       child: Text(
+  //         '1 $value = $finalCurrencyValue $asset_id_quote',
+  //         textAlign: TextAlign.center,
+  //         style: TextStyle(
+  //           fontSize: 20.0,
+  //           color: Colors.white,
+  //         ),
+  //       ),
+  //     ),
+  //   );
   // }
-
-  dynamic generatingWidget() {
-    List<Widget> generatingAWidget = List.generate(
-      cryptoList.length,
-      (index) {
-        String base = cryptoList[index];
-        String finalCurrencyValue = exchangeRates.formattedNumberCurrency;
-
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(18.0, 10.0, 18.0, 0),
-          child: Card(
-            color: Colors.lightBlueAccent,
-            elevation: 5,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              child: Text(
-                '1 $base = $finalCurrencyValue $asset_id_quote',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 19,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            //
-            // textAlign: TextAlign.center,
-          ),
-        );
-      },
-    );
-    return generatingAWidget;
-  }
 
   @override
   Widget build(BuildContext context) {
-    generatingWidget();
-    // generatingWidget().generatingAWidget;
-    // String finalCurrencyValue = exchangeRates.formattedNumberCurrency;
-    // androidDropdown();
+    String finalCurrencyValue = exchangeRates.formattedNumberCurrency;
+    // threeWidgets();
+    androidDropdown();
 
     return Scaffold(
         appBar: AppBar(
           title: Text('🤑 Coin Ticker'),
         ),
-        body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (BuildContext context, int index) {
-            Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
-              generatingWidget(),
-              // ...generatingAWidget,
-              // generatingWidget(),
-              Container(
-                height: 150.0,
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(bottom: 30.0),
-                color: Colors.lightBlue,
-                child: Platform.isAndroid ? androidDropdown() : iOSPicker(), // Ternary operator to find the running OS
+        body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            //Todo: Create this child element
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
-            ]);
-            return generatingWidget();
-          },
-          // child: ,
-        ));
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 $asset_id_base = $finalCurrencyValue $asset_id_quote',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: Platform.isAndroid ? androidDropdown() : iOSPicker(), // Ternary operator to find the running OS
+          )
+        ]));
   }
 }
